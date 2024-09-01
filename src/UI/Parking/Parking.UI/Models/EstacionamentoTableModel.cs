fb9368e3cc8823a604b4ba4650a4bf88cadec125
brenovandall <vbreno335@gmail.com>
2024-09-01 15:20:04 -0300

    Ajustado cultura do valor monetário para reais brasileiros

--- src/UI/Parking/Parking.UI/Models/EstacionamentoTableModel.cs
+++ src/UI/Parking/Parking.UI/Models/EstacionamentoTableModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Parking.UI.Models;
+﻿using System.Globalization;
+
+namespace Parking.UI.Models;
 
 public class EstacionamentoTableModel
 {
@@ -6,6 +8,7 @@ public class EstacionamentoTableModel
     public DateTime DataEntrada { get; set; }
     public DateTime? DataSaida { get; set; }
     public decimal? ValorFinal { get; set; }
+    public string ValorFinalReais => ValorFinal?.ToString("C", new CultureInfo("pt-BR"))!;
     public TimeSpan? Duracao { get; set; }
     public Veiculo? Veiculo { get; set; }
 }
